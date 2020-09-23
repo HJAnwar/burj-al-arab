748fce62504820bfe3fb096d4533ace7140501ec
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-09-23 16:22:35 -0700

    done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4eFJJR3h2uGPJu0OzCPCNKyjhYoDtQE8",
+    authDomain: "burj-al-arab-fcd7c.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-fcd7c.firebaseio.com",
+    projectId: "burj-al-arab-fcd7c",
+    storageBucket: "burj-al-arab-fcd7c.appspot.com",
+    messagingSenderId: "825249845535",
+    appId: "1:825249845535:web:4dad0823f009a918be202a"
+  };
+  export default firebaseConfig;
+  
